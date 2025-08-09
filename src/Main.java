f554d13362e702a4cebd71f9a742dbabd60412fe
Teki-1 <nournabarawi20@gmail.com>
2025-08-09 15:31:57 +0300

    Added dice roll

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main (String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.close();
+
+        Random random = new Random();
+
+        int numOfDice;
+        int total;
+
+        System.out.println("Enter no. of dice you want to roll:");
+        numOfDice=scanner.nextInt();
+        if (numOfDice>0){
+            System.out.println("Rolling dice!");
+            for (int i =0; i<numOfDice; i++){
+                int roll= random.nextInt(1,7);
+                System.out.println("You rolled a "+ roll);
+            }
+        }
+        else{
+            System.out.println("Number must be higher than 0");
+        }
+    }
+
 }
